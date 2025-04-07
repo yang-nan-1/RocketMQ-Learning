42abb6fec59e329eec9575c62cb5253e550ba332
yangnan <1962155093@qq.com>
2025-04-07 23:12:22 +0800

    创建第一个练习Demo

---
+++ rocketMQ-demo1/src/test/java/com/rmq/RocketMqDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketMqDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
