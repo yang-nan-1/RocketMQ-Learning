7c33827643d76b900e20591d03cb0937d7912aa9
yangnan <1962155093@qq.com>
2025-04-09 15:11:10 +0800

    批量发送消息

--- rocketMQ-demo1/src/main/java/com/rmq/demo/BatchTest.java
+++ rocketMQ-demo1/src/main/java/com/rmq/demo/BatchTest.java
@@ -1,13 +1,19 @@
 package com.rmq.demo;
 
 import com.rmq.constant.MqConstant;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 public class BatchTest {
@@ -25,5 +31,22 @@ public class BatchTest {
         SendResult send = producer.send(messages);
         System.out.println(send);
         producer.shutdown();
+    }
+
+    @Test
+    public void batchConsumer() throws Exception{
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("batch-consumer-group");
+        consumer.setNamesrvAddr(MqConstant.NAME_SERVER);
+        consumer.subscribe("batchTopic","*");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                System.out.println("收到消息了"+new Date());
+                System.out.println(new String(msgs.get(0).getBody()));
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+        System.in.read();
     }
 }
