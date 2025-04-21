69178c11bb33bf9877fe555e8d451742433e5ed3
yangnan <1962155093@qq.com>
2025-04-21 11:32:46 +0800

    对业务流程解耦，写成异步操作，对数据库的io操作放到service中通过mq向service发送消息，完成io操作，可提升接口的qts

---
+++ seckill-web/src/main/java/com/rmq/seckill/SeckillWebApplication.java
@@ -0,0 +1,13 @@
+package com.rmq.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillWebApplication.class, args);
+    }
+
+}
---
+++ seckill-web/src/test/java/com/rmq/seckill/SeckillWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmq.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
